7b3ccab6a70e507947ec27a11a6059bc4f97315c
dbarowy <GitHub4484?>
2015-05-30 10:27:28 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "PLDI_assignments"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+
+libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.2.1"
+libraryDependencies += "joda-time" % "joda-time" % "2.7"
---
+++ src/main/scala/PLDIAssignments.scala
@@ -0,0 +1,92 @@
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.TimeZone
+import com.github.tototoshi.csv._
+import org.joda.time.{DateTimeZone, LocalDateTime}
+
+object PLDIAssignments extends App {
+  if (args.size == 0) {
+    println("Usage:")
+    println("\tsbt \"run <users.csv> <assignments.csv>\"")
+    sys.exit(1)
+  }
+
+  val csv_users = args(0)
+  val csv_assignments = args(1)
+
+  val reader_users = CSVReader.open(new File(csv_users))
+  val users: List[Map[String, String]] = reader_users.allWithHeaders()
+  reader_users.close()
+
+  val user_db = users.map { row =>
+    val vol_id = row("id")
+    val last = row("last")
+    val first = row("first")
+    val email = row("email")
+    val gender = row("gender")
+    vol_id -> Map('first -> first, 'last -> last, 'gender -> gender, 'email -> email)
+  }.toMap
+
+  val reader_assignments = CSVReader.open(new File(csv_assignments))
+  val assignments: List[Map[String, String]] = reader_assignments.allWithHeaders()
+  reader_assignments.close()
+
+  // define date format parser
+  val df = new SimpleDateFormat("dd.MM.yyyy kk:mm zzzz")
+
+  // for each user, find all of that user's assignments
+  // want:
+  // user -> List((assignment1,time1),...,(assignmentn,timen))
+  val user_assignments = users.flatMap { row =>
+    val vol_id = row("id")
+
+    val assns = assignments.flatMap { arow =>
+      val volunteers = arow("assigned_volunteers").split(";")
+      if(volunteers.contains(vol_id)) {
+        // parse dates & convert to PDT
+        val start_d = date_conv(df.parse(arow("start")))
+        val end_d = date_conv(df.parse(arow("end")))
+
+        // unfortunately, the string representation is still wrong
+        val start_conv = start_d.toString.replace("EDT","PDT")
+        val end_conv = end_d.toString.replace("EDT","PDT")
+
+        // compute duration in mins
+        val duration = (end_d.getTime - start_d.getTime)/1000/60
+
+        Some((arow("event"),start_conv,end_conv,duration))
+      } else {
+        None
+      }
+    }
+
+    if (assns.isEmpty) {
+      None
+    } else {
+      Some(vol_id -> assns)
+    }
+  }
+
+  user_assignments.foreach { case (vol_id, assns) =>
+
+    val total_duration = assns.foldLeft(0L){ case (acc,(_,_,_,duration)) => acc + duration}
+
+    println(user_db(vol_id)('first) +
+            " " + user_db(vol_id)('last) +
+            " <" + user_db(vol_id)('email) + ">" +
+            " (" + total_duration + " total minutes)"
+    )
+    assns.foreach { case(event, start, end, duration) =>
+      println("\t" + start + " UNTIL " + end + " (" + duration + " minutes)" + "\t" + event)
+    }
+
+    println()
+  }
+
+  def date_conv(date: java.util.Date) : java.util.Date = {
+    val start_dj = new LocalDateTime(date)
+    val srcDateTime = start_dj.toDateTime(DateTimeZone.forID("US/Eastern"))
+    val dstDateTime = srcDateTime.withZone(DateTimeZone.forID("US/Pacific"))
+    dstDateTime.toLocalDateTime.toDateTime.toDate
+  }
+}
